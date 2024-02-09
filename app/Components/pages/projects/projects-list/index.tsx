97961ea68a18edc048e60f24f068175a3c186035
fsRoger <fsroger@outlook.com>
2024-02-08 21:40:46 -0300

    add psicologa site

--- app/Components/pages/projects/projects-list/index.tsx
+++ app/Components/pages/projects/projects-list/index.tsx
@@ -14,8 +14,7 @@ type ProjectsListProps = {
 export const ProjectsList = ({ projects }: ProjectsListProps) => {
   return (
     <section
-      className='container py-32 grid grid-cols-1 sm:grid-cols-[repeat(auto-fit
-        ,minmax(350px,1fr))] gap-x-4 gap-y-6'
+      className='container py-32 grid grid-cols-1 sm:grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-x-4 gap-y-6'
     >
       {projects.map((project, i) => (
         <motion.div
--- app/Components/pages/projects/projects-list/project-card/index.tsx
+++ app/Components/pages/projects/projects-list/project-card/index.tsx
@@ -8,7 +8,7 @@ type ProjectCardProps = {
 export const ProjectCard = ({ project }: ProjectCardProps) => {
   const technologies = project.technologies.map(x => x.name).join(', ');
   return (
-    <div className='rounded-lg h-[436px]f flex flex-col bg-gray-800 overflow-hidden border-2 border-gray-800 hover:border-emerald-500 opacity-70 hover:opacity-100 transition-all group'>
+    <div className='rounded-lg h-[436px] flex flex-col bg-gray-800 overflow-hidden border-2 border-gray-800 hover:border-emerald-500 opacity-70 hover:opacity-100 transition-all group'>
       <div className="w-full h-48 overflow-hidden" >
         <Image
           width={380}
